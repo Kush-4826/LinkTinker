8396979050a4f95cbf80eaf3df86df4380670e0d
Kush Agarwal <kush.301104@gmail.com>
2025-07-11 20:32:06 +0530

    Created Layouts

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="absolute bottom-0 left-0 right-0 py-5 flex justify-center">
+    <p class="text-gray-500 text-sm font-syne">Built From Bytes By <a href="https://github.com/Kush-4826" target="_blank" class="text-white hover:underline">Kush</a></p>
+</footer>
---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,6 @@
+<header class="lg:absolute w-full lg:max-w-4xl max-w-[335px] text-sm mb-6">
+    <div class="text-white font-syne flex flex-col items-start">
+        <h1 class="font-bold text-2xl">LinkTinker</h1>
+        <p class="text-[14px] text-gray-500">The URL Shortener powered by Laravel</p>
+    </div>
+</header>
