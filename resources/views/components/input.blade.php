537823cdc04a580e8b59585bac8ec1109f65977e
Kush Agarwal <kush.301104@gmail.com>
2025-07-11 20:52:14 +0530

    new: input component

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,1 @@
+<input type="{{ $type }}" placeholder="{{ $placeholder }}" class="font-syne text-white p-2 rounded-md border border-[#fffaed2d] focus:outline-0 focus:border-white" name="{{ $name }}" />
