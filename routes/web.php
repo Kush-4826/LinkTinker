59b44d3d5bb948bb56ac946a1f075c82c5bb1e2d
Kush Agarwal <kush.301104@gmail.com>
2025-07-12 22:04:26 +0530

    refactor: changed route order

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\ShortLinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ShortLinkController::class, 'create'])->name('shortlink.create');
-Route::get("/{shortlink:slug}", [ShortLinkController::class, 'show'])->name('shortlinks.show');
 Route::post("/shortlinks", [ShortLinkController::class, 'store'])->name('shortlinks.store');
+Route::get("/{shortlink:slug}", [ShortLinkController::class, 'show'])->name('shortlinks.show');
