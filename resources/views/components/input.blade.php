5f67fd137c719cc62c75e24a1b2d2bf7db00dba7
Kush Agarwal <kush.301104@gmail.com>
2025-07-12 09:05:53 +0530

    refactor: Added Label to input

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,1 +1,4 @@
-<input type="{{ $type }}" placeholder="{{ $placeholder }}" class="font-syne text-white p-2 rounded-md border border-[#fffaed2d] focus:outline-0 focus:border-white" name="{{ $name }}" />
+<div>
+    <label for="{{ $name }}" class="font-syne text-white text-sm font-bold mb-1 inline-block">{{ $label }}</label>
+    <input type="{{ $type }}" placeholder="{{ $placeholder }}" class="font-syne text-white p-2 rounded-md border border-[#fffaed2d] focus:outline-0 focus:border-white w-full" name="{{ $name }}" />
+</div>
